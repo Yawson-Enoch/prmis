fb433be025635ca1aa77083a6c014d8910db9cf6
Yawson-Enoch <enochgybex25@gmail.com>
2022-10-03 18:31:17 +0000

    chore: remove Logo component animation

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,25 +1,18 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './Logo.module.scss';
-import { motion } from 'framer-motion';
-import { scaleDown } from '../../animations/animations';
 
 const Logo = () => {
   return (
-    <motion.div
-      className={styles.companyInfo}
-      variants={scaleDown}
-      initial="initial"
-      animate="animate"
-    >
+    <div className={`center ${styles.companyInfo}`}>
       <div className={styles.logo}>
         <Link href="/">
           <a className="center">
             <Image
               src="/assets/cover/logo.png"
               alt="Obeyeyie Medical Center logo"
-              width={45}
-              height={45}
+              width={40}
+              height={40}
             ></Image>
           </a>
         </Link>
@@ -30,7 +23,7 @@ const Logo = () => {
 
         <p>We prioritize your health</p>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
